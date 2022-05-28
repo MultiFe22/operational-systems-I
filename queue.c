31d3ab1849a5184f1b01f0e4bf6ebd79050da55e
André Gaeta <andremg@dcc.ufrj.br>
2022-05-28 20:46:55 -0300

    fixed queue header

--- queue.c
+++ queue.c
@@ -1,26 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct _QueuedProcess 
-{
-    Process* process;
-    struct _QueuedProcess *prev;
-} QueuedProcess;
-
-typedef struct _Queue 
-{
-    QueuedProcess* head;
-    QueuedProcess* tail;
-    int size;
-    int max;
-} Queue;
-
-Queue* createQueue(int max);
-void freeQueue(Queue *queue);
-int enqueue(Queue *queue, Process *item);
-Process* dequeue(Queue *queue);
-int isEmpty(Queue* queue);
-
 Queue* createQueue(int max) 
 {
     Queue *queue = (Queue*) malloc(sizeof (Queue));
